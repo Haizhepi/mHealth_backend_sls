bb218ee3fe86cf53485da69f2cb64f89562bd4b6
Liu <Yunzhe_Liu@baylor.edu>
2020-05-01 01:22:02 +0000

    fix 1

---
+++ api/add-blood-pressure.js
@@ -0,0 +1,50 @@
+/**
+ * Route: POST /location
+ * 
+ * 
+ */
+
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+const moment = require('moment');
+const uuidv4 = require('uuid/v4');
+const util = require('./util.js');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.BPD1;
+
+exports.handler = async (event) => {
+    try {
+        let item  = JSON.parse(event.body).Item;
+        item.user_id = util.getUserId(event.headers);
+        item.user_name = util.getUserName(event.headers);
+        item.timestamp = moment().unix();
+
+        if (!item.high || !item.low) {
+            throw new Error("missing high or low")
+        }
+
+        let data = await dynamodb.put({
+            TableName: tableName,
+            Item: item 
+        }).promise();
+
+        return {
+            statusCode: 200,
+            headers: util.getResponseHeaders(),
+            body: JSON.stringify(item)
+        };
+    }
+    catch (err) {
+        console.log("Error", err);
+        return {
+            statusCode: err.statusCode ? err.statusCode : 500,
+            headers: util.getResponseHeaders(),
+            body: JSON.stringify( {
+                error: err.name ? err.name : "exception",
+                message: err.message ? err.message : "unknown error"
+            })
+        };
+    }
+}
